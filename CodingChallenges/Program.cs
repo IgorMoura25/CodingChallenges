d2eaa3893304fef208d7ba087af3002e98f93635
Igor Moura <igor.moura254@hotmail.com>
2022-12-29 20:34:18 -0300

    Implementing a tree

--- CodingChallenges/Program.cs
+++ CodingChallenges/Program.cs
@@ -1,4 +1,6 @@
 ﻿using CodingChallenges.Arrays;
+using CodingChallenges.Trees.BinarySearchTree;
+using System.Collections;
 
 HashTableChallenges.CallReturnFirstRecurringNumber();
 
@@ -8,5 +10,20 @@ ArrayChallenges.CallDiagonalDifference();
 ArrayChallenges.Staircase(6);
 ArrayChallenges.CallMiniMaxSum();
 ArrayChallenges.CallBirthdayCakeCandles();
+
+var tree = new BinarySearchTree();
+var lookup1 = tree.Lookup(9);
+
+tree.Insert(9);
+tree.Insert(4);
+tree.Insert(6);
+tree.Insert(20);
+tree.Insert(170);
+tree.Insert(15);
+tree.Insert(1);
+
+var lookup2 = tree.Lookup(9);
+var lookup3 = tree.Lookup(170);
+var lookup4 = tree.Lookup(50);
 
 Console.ReadKey();
---
+++ CodingChallenges/Trees/BinarySearchTree/BinarySearchTree.cs
@@ -0,0 +1,92 @@
+﻿namespace CodingChallenges.Trees.BinarySearchTree
+{
+    public class BinarySearchTree
+    {
+        public Node? Root { get; set; }
+
+        public void Insert(int value)
+        {
+            if (Root == null)
+            {
+                Root = new Node(value);
+                return;
+            }
+
+            var currentNode = Root;
+
+            while (true)
+            {
+                if (value < currentNode.Value && currentNode.Left == null)
+                {
+                    currentNode.Left = new Node(value);
+                    break;
+                }
+
+                if (value < currentNode.Value && currentNode.Left != null)
+                {
+                    currentNode = currentNode.Left;
+                    continue;
+                }
+
+                if (value > currentNode.Value && currentNode.Right == null)
+                {
+                    currentNode.Right = new Node(value);
+                    break;
+                }
+
+                if (value > currentNode.Value && currentNode.Right != null)
+                {
+                    currentNode = currentNode.Right;
+                    continue;
+                }
+
+                break;
+            }
+        }
+
+        public Node? Lookup(int value)
+        {
+            if (Root == null) return null;
+
+            var currentNode = Root;
+
+            while (true)
+            {
+                if (currentNode.Value == value) return currentNode;
+                if (currentNode.Left?.Value == value) return currentNode.Left;
+                if (currentNode.Right?.Value == value) return currentNode.Right;
+
+                if (value < currentNode.Value && currentNode.Left != null)
+                {
+                    currentNode = currentNode.Left;
+                    continue;
+                }
+
+                if (value > currentNode.Value && currentNode.Right != null)
+                {
+                    currentNode = currentNode.Right;
+                    continue;
+                }
+
+                return null;
+            }
+        }
+
+        public void Remove(int value)
+        {
+
+        }
+    }
+
+    public class Node
+    {
+        public int? Value { get; }
+        public Node? Left { get; set; }
+        public Node? Right { get; set; }
+
+        public Node(int value)
+        {
+            Value = value;
+        }
+    }
+}
