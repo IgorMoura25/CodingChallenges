bfc6b41bcce315769ada62994a1b5e79f1c06e57
Igor Moura <igor.moura254@hotmail.com>
2023-01-08 10:53:49 -0300

    Memoized fibonacci sequence

--- CodingChallenges/Algorithms/RecursionAlgorithms.cs
+++ CodingChallenges/Algorithms/RecursionAlgorithms.cs
@@ -2,14 +2,21 @@
 {
     public static class RecursionAlgorithms
     {
+        public static int Iterations { get; private set; }
+
         // Fibonnaci: 0, 1, 1, 2, 3, 5, 8, 13, 21
         // index = 6
         // return = 8
         public static int GetValueOfFibonacciByIndex(int index)
         {
+            Iterations++;
+
             if (index < 2) return index;
 
-            return GetValueOfFibonacciByIndex(index - 1) + GetValueOfFibonacciByIndex(index - 2);
+            var a = GetValueOfFibonacciByIndex(index - 1);
+            var b = GetValueOfFibonacciByIndex(index - 2);
+
+            return a + b;
         }
 
         // Fibonnaci: 0, 1, 1, 2, 3, 5, 8, 13, 21
@@ -35,6 +42,34 @@
 
                 iterations++;
             }
+
+            Iterations = iterations;
+            return result;
+        }
+
+        public static int FibonacciMemoizedMaster(int index)
+        {
+            var cache = new Dictionary<int, int>();
+
+            var result = FibonacciMemoized(index, cache);
+
+            return result;
+        }
+
+        public static int FibonacciMemoized(int index, Dictionary<int, int> cache)
+        {
+            Iterations++;
+
+            if (cache.ContainsKey(index)) return cache[index];
+
+            if (index < 2) return index;
+
+            var a = FibonacciMemoized(index - 1, cache);
+            var b = FibonacciMemoized(index - 2, cache);
+
+            var result = a + b;
+
+            cache.Add(index, result);
 
             return result;
         }
--- CodingChallenges/Program.cs
+++ CodingChallenges/Program.cs
@@ -16,8 +16,14 @@ TreesChallenges.RunBinarySearchTree();
 
 GraphsChallenges.RunGraphs();
 
-var teste = RecursionAlgorithms.GetValueOfFibonacciByIndex(6);
-var teste2 = RecursionAlgorithms.GetValueOfFibonacciByIndexIterative(6);
+var teste = RecursionAlgorithms.GetValueOfFibonacciByIndex(10);
+var testeIterations = RecursionAlgorithms.Iterations;
+
+var teste2 = RecursionAlgorithms.GetValueOfFibonacciByIndexIterative(10);
+var teste2Iterations = RecursionAlgorithms.Iterations;
+
+var teste3 = RecursionAlgorithms.FibonacciMemoizedMaster(10);
+var teste3Iterations = RecursionAlgorithms.Iterations;
 
 SortingAlgorithms.CallBubbleSort();
 SortingAlgorithms.CallMergeSort();
